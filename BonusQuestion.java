6863b50ded5bab82c4b84e5bcfaa23b7822f5c4f
ARSH5588 <60395168+ARSH5588@users.noreply.github.com>
2020-05-02 11:41:24 -0400

    BonusQuestion

--- BonusQuestion.java
+++ BonusQuestion.java
@@ -2,21 +2,8 @@
 public class BonusQuestion {
   public static void main(String[] args) {
 
-    Myname name = new Myname("Arshjit");
-    System.out.println("My name is " + name.getMyname());
+    Question name = new Question("Arshjit");
+    System.out.println("My name is " + name.getQuestion());
 
-  }
-}
-class Myname {
-  String name = "Arshjit";
-
-  public Myname(String n) {
-    name = n;
-  }
-  public void setMyname(String n) {
-    name = n;
-  }
-  public String getMyname() {
-    return name;
   }
 }
---
+++ Question.java
@@ -0,0 +1,14 @@
+class Question {
+
+  String name = "Arshjit";
+
+  public Question(String n) {
+    name = n;
+  }
+  public void setQuestion(String n) {
+    name = n;
+  }
+  public String getQuestion() {
+    return name;
+  }
+}
